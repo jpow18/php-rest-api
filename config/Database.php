e79e419276199757568b42c21e6c00ee6b89763b
James Pow <jbpow18@icloud.com>
2023-02-23 11:33:06 -0500

    fix/change-password-env-variable and database-to-postgres

--- config/Database.php
+++ config/Database.php
@@ -1,20 +1,29 @@
 <?php
 
   class Database {
+
+    
     // DB Params
     private $host = 'localhost';
+    private $port = '5432';
     private $db_name = 'myblog';
-    private $username = 'root';
-    private $password = 'sdmV[TQqecJ@8_u4';
+    private $username = 'postgres';
+    private $password;
     private $conn;
+
+    // Use constructor to set password as it is not
+    // constant and so cannot be set above.
+    public function __construct() {
+      $this->password = getenv('DB_PASSWORD');
+    }
 
     // DB Connect
     public function connect() {
       $this->conn = null;
+      $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
 
       try {
-        $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db_name",
-      $this->username, $this->password);
+        $this->conn = new PDO($dsn, $this->username, $this->password);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch (PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
